1b6e5f77be2a43126dbf5b8ce232820230620c9a
Михаил Ю. Гоженко <mgozhenko@yoomoney.ru>
2023-07-22 18:02:49 +0300

    added traffic light and switch Button

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var redLightView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 60
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = 60
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 60
+        greenLightView.alpha = 0.3
     }
-
-
+    
+    @IBAction func changeColor(_ sender: UIButton) {
+        redLightView.alpha = 1
+        sender.setTitle("NEXT", for: .normal)
+    }
 }
 
